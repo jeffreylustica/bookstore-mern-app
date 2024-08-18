6764ae233bf9ee06ad1944fdd046899bd2fb201a
jeff <jefflustica2@gmail.com>
2024-08-18 13:51:46 +0800

    Edit Book page updated

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -35,7 +35,7 @@ const CreateBooks = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Create Book</h1>
+      <h1 className="text-3xl my-4">Edit Book</h1>
       {isloading ? <Spinner /> : ""}
 
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
